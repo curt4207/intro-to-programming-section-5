1cd49cc78b294d866cdd8b0b338ed34bb643e2ec
Curtis <Curt122181@gmail.com>
2021-11-29 09:57:16 -0500

    Updated file.

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function checkGuess() {
   if (attempts === maxNumberOfAttempts) {
     submitButton.disabled = true;
     guessInput.disabled = true;
-  }if(attempts != 1){
+  }if(attempts === 4){
     numberOfGuessesMessage.innerHTML =`you guessed ${guess}. <br>  
     1 guess remaining`;
   } 
